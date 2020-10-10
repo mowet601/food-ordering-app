d5afc256d4406b9cb8c9dd7b0e9a142ce86cbc28
rubenrudov <59509727+rubenrudov@users.noreply.github.com>
2020-10-10 20:18:11 +0300

    Splash screen addition
    Splash screen with the theme picture,
     temporal splash screen,
    will be changed later with the final screen.
    the assets are:
    - assets/images
    - assets/icons

--- main.dart
+++ main.dart
@@ -4,21 +4,45 @@ import 'package:flutter/material.dart';
 import 'package:food_ordering/pages/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    )
+  );
 }
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget{
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState(){
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 4),
+      () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+      }
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ביס ראשון',
-      theme: ThemeData(
-        primaryColor: Colors.deepPurpleAccent,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Home(),
-
+    return Scaffold(
+        body: Container(
+            child: Image(
+              image: AssetImage('assets/images/themePic.jpg'),
+              fit: BoxFit.fill,
+              height: double.infinity,
+              alignment: Alignment.center,
+            ),
+        )
     );
   }
 }
