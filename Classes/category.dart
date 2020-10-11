03fa5389161072d10435c1b1853845da842a5823
rubenrudov <59509727+rubenrudov@users.noreply.github.com>
2020-10-11 20:39:14 +0300

    Classes folder
    The classes in this folder are Models for the ListView widgets.

---
+++ Classes/category.dart
@@ -0,0 +1,9 @@
+class Category{
+  final String cName;
+  final String icon;
+
+  Category({this.cName, this.icon});
+  // Note: constructor is not requires a parameter
+  // Optional addition: @required before:
+  // ..@required this.cName, @required this.icon..
+}
