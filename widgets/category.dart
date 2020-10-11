2f411f45ad57ea3ab1b06f58ceb139a09df59f28
rubenrudov <59509727+rubenrudov@users.noreply.github.com>
2020-10-11 20:37:25 +0300

    Moved
    Moved to Classes folder

--- widgets/category.dart
+++
@@ -1,9 +0,0 @@
-class Category{
-  final String cName;
-  final String icon;
-
-  Category({this.cName, this.icon});
-  // Note: constructor is not requires a parameter
-  // Optional addition: @required before:
-  // ..@required this.cName, @required this.icon..
-}
