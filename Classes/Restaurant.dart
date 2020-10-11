aec19f27dbb751c0161e10c47acd7a21808798b9
rubenrudov <59509727+rubenrudov@users.noreply.github.com>
2020-10-11 21:31:05 +0300

    Restaurant class

--- Classes/Restaurant.dart
+++ Classes/Restaurant.dart
@@ -1,3 +1,12 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
+// Model class for Restaurants list & ListView
+
+class Restaurant{
+  final String rName;
+  final String picturePath;
+  final String type;
+
+  Restaurant({@required this.rName, @required this.picturePath, @required this.type});
+}
 
