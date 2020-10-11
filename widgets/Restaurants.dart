b1899bfd586f02bc8b69e1273f15228ed5ae4e72
rubenrudov <59509727+rubenrudov@users.noreply.github.com>
2020-10-12 00:32:14 +0300

    Update
    Functional List of Restaurants will be replaced with database data later.

--- widgets/Restaurants.dart
+++ widgets/Restaurants.dart
@@ -6,14 +6,14 @@ import 'semi_titles.dart';
 
 // If data base is used' change it to get names and images for it
 List<Restaurant> restaurantsLst = [
-  Restaurant(rName: 'הבורגר הראשון', picturePath:'burger.png', type: "Burger"),
-  Restaurant(rName: 'פיצה די-איטליה', picturePath:'pizza.png', type: "Italian"),
-  Restaurant(rName: 'סטייק-האוס', picturePath:'steak.png', type: "Steakhouse"),
-  Restaurant(rName: 'מאקי', picturePath:'sushi.png', type: "Asian"),
-  Restaurant(rName: 'הנודלייה', picturePath:'noodles.png', type: "Asian"),
-  Restaurant(rName: 'לחם המרכז', picturePath:'pastry.png', type: "Bakery"),
-  Restaurant(rName: 'פיצה פסטה', picturePath:'pasta.png', type: "Italian"),
-  Restaurant(rName: 'מיסטר בין', picturePath:'beer.png', type: "Pub"),
+  Restaurant(rName: 'הבורגר הראשון', picturePath:'burger2.jpg', type: "Burger"),
+  Restaurant(rName: 'פיצה די-איטליה', picturePath:'pizza2.jpg', type: "Italian"),
+  Restaurant(rName: 'סטייק-האוס', picturePath:'steak2.jpg', type: "Steakhouse"),
+  Restaurant(rName: 'מאקי', picturePath:'sushi2.jpg', type: "Asian"),
+  Restaurant(rName: 'הנודלייה', picturePath:'noodles2.jpg', type: "Asian"),
+  Restaurant(rName: 'לחם המרכז', picturePath:'pastry2.jpg', type: "Bakery"),
+  Restaurant(rName: 'פיצה פסטה', picturePath:'pasta2.jpg', type: "Italian"),
+  Restaurant(rName: 'מיסטר בין', picturePath:'beer2.jpg', type: "Pub"),
 ];
 
 
@@ -21,16 +21,34 @@ class Restaurants extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 120,
-      child: ListView.builder(
+      height: 420,
+        child: ListView.builder(
           itemCount: restaurantsLst.length,
-          scrollDirection: Axis.horizontal,
+          scrollDirection: Axis.vertical,
           itemBuilder: (_, index){
-            return Column(
-
+            return Padding(
+            padding: EdgeInsets.fromLTRB(7, 8, 7, 0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Stack(
+                    children: [
+                      Image.asset("assets/images/${restaurantsLst[index].picturePath}"),
+                      Center(
+                          child: Text(
+                            "${restaurantsLst[index].rName}",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.w700
+                            ),
+                          )
+                      )
+                    ]
+                )
+              )
             );
-          }
-      ),
+        }
+      )
     );
   }
 }
