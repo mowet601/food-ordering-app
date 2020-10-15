78cc0823ef78e3ce7ee307f0ae6f3a2a72653f71
rubenrudov <59509727+rubenrudov@users.noreply.github.com>
2020-10-15 09:55:00 +0300

    Update and rename Restaurants.dart to Dishes.dart

--- widgets/Dishes.dart
+++ widgets/Dishes.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:food_ordering/colorScheme.dart';
-import 'file:///D:/uiTraining07102020/food_ordering/lib/Classes/Restaurant.dart';
+import 'file:///D:/uiTraining07102020/food_ordering/lib/Classes/Dish.dart';
 import 'semi_titles.dart';
 
 
 // If data base is used' change it to get names and images for it
-List<Restaurant> restaurantsLst = [
-  Restaurant(rName: 'הבורגר הראשון', picturePath:'burger2.jpg', type: "Burger"),
-  Restaurant(rName: 'פיצה די-איטליה', picturePath:'pizza2.jpg', type: "Italian"),
-  Restaurant(rName: 'סטייק-האוס', picturePath:'steak2.jpg', type: "Steakhouse"),
-  Restaurant(rName: 'מאקי', picturePath:'sushi2.jpg', type: "Asian"),
-  Restaurant(rName: 'הנודלייה', picturePath:'noodles2.jpg', type: "Asian"),
-  Restaurant(rName: 'לחם המרכז', picturePath:'pastry2.jpg', type: "Bakery"),
-  Restaurant(rName: 'פיצה פסטה', picturePath:'pasta2.jpg', type: "Italian"),
-  Restaurant(rName: 'מיסטר בין', picturePath:'beer2.jpg', type: "Pub"),
+List<Dish> restaurantsLst = [
+  Dish(rName: 'הבורגר הראשון', picturePath:'burger2.jpg', type: "Burger"),
+  Dish(rName: 'פיצה די-איטליה', picturePath:'pizza2.jpg', type: "Italian"),
+  Dish(rName: 'סטייק-האוס', picturePath:'steak2.jpg', type: "Steakhouse"),
+  Dish(rName: 'מאקי', picturePath:'sushi2.jpg', type: "Asian"),
+  Dish(rName: 'הנודלייה', picturePath:'noodles2.jpg', type: "Asian"),
+  Dish(rName: 'לחם המרכז', picturePath:'pastry2.jpg', type: "Bakery"),
+  Dish(rName: 'פיצה פסטה', picturePath:'pasta2.jpg', type: "Italian"),
+  Dish(rName: 'מיסטר בין', picturePath:'beer2.jpg', type: "Pub"),
 ];
 
 
-class Restaurants extends StatelessWidget{
+class Dishes extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
